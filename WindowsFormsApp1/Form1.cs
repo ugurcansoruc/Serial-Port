5916fcdcfcdc156d96127a133b09c27199ec69a2
Uğurcan Soruç <ugurcan.soruc@gmail.com>
2020-11-10 16:10:37 +0300

    Port select errors is corrected

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,42 +7,37 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        //continue kaldırıldı
         static SerialPort _serialPort;
-        //static StringComparer _stringComparer;
-        //static Thread _readThread;
+ 
         public Form1()
         {
             InitializeComponent();
-            _serialPort = new SerialPort();
-            //_stringComparer = StringComparer.OrdinalIgnoreCase;
-            //_readThread = new Thread(Read);
         }
-        public void Form1_Load(object sender, EventArgs e)
+        public void InitializeCombobox()
         {
-            
             string[] baudRates = new string[] { "19200", "9600" };
             BaudRateComboBox.Items.AddRange(baudRates);
             BaudRateComboBox.SelectedIndex = 1;
 
-            string[] dataBits = new string[] { "5", "6","7","8"};
+            string[] dataBits = new string[] { "5", "6", "7", "8" };
             DataBitComboBox.Items.AddRange(dataBits);
             DataBitComboBox.SelectedIndex = 3;
 
-     
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                PortNameComboBox.Items.Add(s);
+            }
+            PortNameComboBox.SelectedIndex = 1;
 
-            //_serialPort = new SerialPort();
-
-            _serialPort.PortName = SetPortName(_serialPort.PortName, PortNameComboBox);
-            _serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate, BaudRateComboBox);
-            _serialPort.Parity = SetPortParity(_serialPort.Parity, ParityComboBox);
-            _serialPort.DataBits = SetPortDataBits(_serialPort.DataBits, DataBitComboBox);
-
-            //bunları eklemesek de olur
-            _serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
-            _serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
-
-            _serialPort.ReadTimeout = 500;
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                ParityComboBox.Items.Add(s);
+            }
+            ParityComboBox.SelectedIndex = 0;
+        }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeCombobox();
         }
 
         delegate void SetTextCallback(string text);
@@ -74,13 +69,8 @@ namespace WindowsFormsApp1
         public static string SetPortName(string defaultPortName,ComboBox comboBox)
         {
             string portName;
-
-            foreach (string s in SerialPort.GetPortNames())
-            {
-                comboBox.Items.Add(s);
-            }
-            comboBox.SelectedIndex = 1;
-            portName = comboBox.SelectedIndex.ToString();
+            
+            portName = comboBox.SelectedItem.ToString();
 
             if (portName == "" || !(portName.ToLower()).StartsWith("com"))
             {
@@ -106,14 +96,8 @@ namespace WindowsFormsApp1
         public static Parity SetPortParity(Parity defaultPortParity, ComboBox comboBox)
         {
             string parity;
-
-            foreach (string s in Enum.GetNames(typeof(Parity)))
-            {
-                comboBox.Items.Add(s);
-            }
-            comboBox.SelectedIndex = 0; 
-
-            parity = comboBox.SelectedIndex.ToString();
+            
+            parity = comboBox.SelectedItem.ToString();
 
             if (parity == "")
             {
@@ -141,18 +125,6 @@ namespace WindowsFormsApp1
         {
             string stopBits = "";
 
-            //bu kısım duruma göre ayarlanabilir !!!
-            /*
-            Console.WriteLine("Available StopBits options:");
-            foreach (string s in Enum.GetNames(typeof(StopBits)))
-            {
-                Console.WriteLine("   {0}", s);
-            }
-            
-            Console.Write("Enter StopBits value (None is not supported and \n" +
-             "raises an ArgumentOutOfRangeException. \n (Default: {0}):", defaultPortStopBits.ToString());
-            stopBits = Console.ReadLine();
-            */
             if (stopBits == "")
             {
                 stopBits = defaultPortStopBits.ToString();
@@ -163,17 +135,7 @@ namespace WindowsFormsApp1
         public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
         {
             string handshake ="";
-            // burası duruma göre ayarlanabilir
-            /*
-            Console.WriteLine("Available Handshake options:");
-            foreach (string s in Enum.GetNames(typeof(Handshake)))
-            {
-                Console.WriteLine("   {0}", s);
-            }
-
-            Console.Write("Enter Handshake value (Default: {0}):", defaultPortHandshake.ToString());
-            handshake = Console.ReadLine();
-            */
+            
             if (handshake == "")
             {
                 handshake = defaultPortHandshake.ToString();
@@ -184,20 +146,28 @@ namespace WindowsFormsApp1
 
         private void StartButton_Click(object sender, EventArgs e)
         {
+            _serialPort = new SerialPort();
+
+            _serialPort.PortName = SetPortName(_serialPort.PortName, PortNameComboBox);
+            _serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate, BaudRateComboBox);
+            _serialPort.Parity = SetPortParity(_serialPort.Parity, ParityComboBox);
+            _serialPort.DataBits = SetPortDataBits(_serialPort.DataBits, DataBitComboBox);
+
+            //bunları eklemesek de olur
+            _serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
+            _serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
+
+            _serialPort.ReadTimeout = 500;
+
             _serialPort.DataReceived += new SerialDataReceivedEventHandler(Read);
             _serialPort.Open();
 
-            _continue = true;
             ControlLabel.Text = "Acik";
-            //_readThread.Start();
         }
 
         private void StopButton_Click(object sender, EventArgs e)
         {
-            //_readThread.Join();
             ControlLabel.Text = "Kapali";
-
-            _continue = false;
             _serialPort.Close();
         }
     }
