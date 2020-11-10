7f0987013a07322d0a60443a51ef3911ca4eddca
Uğurcan Soruç <ugurcan.soruc@gmail.com>
2020-11-10 14:45:52 +0300

    Project is added

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.PortNameComboBox = new System.Windows.Forms.ComboBox();
+            this.PortNameLabel = new System.Windows.Forms.Label();
+            this.BaudRateLabel = new System.Windows.Forms.Label();
+            this.BaudRateComboBox = new System.Windows.Forms.ComboBox();
+            this.ParityLabel = new System.Windows.Forms.Label();
+            this.ParityComboBox = new System.Windows.Forms.ComboBox();
+            this.DataBitLabel = new System.Windows.Forms.Label();
+            this.DataBitComboBox = new System.Windows.Forms.ComboBox();
+            this.DataTextBox = new System.Windows.Forms.TextBox();
+            this.ControlLabel = new System.Windows.Forms.Label();
+            this.DataLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(22, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(141, 61);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(22, 79);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(141, 61);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // PortNameComboBox
+            // 
+            this.PortNameComboBox.FormattingEnabled = true;
+            this.PortNameComboBox.Location = new System.Drawing.Point(210, 49);
+            this.PortNameComboBox.Name = "PortNameComboBox";
+            this.PortNameComboBox.Size = new System.Drawing.Size(121, 24);
+            this.PortNameComboBox.TabIndex = 2;
+            // 
+            // PortNameLabel
+            // 
+            this.PortNameLabel.AutoSize = true;
+            this.PortNameLabel.Location = new System.Drawing.Point(207, 12);
+            this.PortNameLabel.Name = "PortNameLabel";
+            this.PortNameLabel.Size = new System.Drawing.Size(75, 17);
+            this.PortNameLabel.TabIndex = 3;
+            this.PortNameLabel.Text = "Port Name";
+            this.PortNameLabel.UseWaitCursor = true;
+            // 
+            // BaudRateLabel
+            // 
+            this.BaudRateLabel.AutoSize = true;
+            this.BaudRateLabel.Location = new System.Drawing.Point(362, 12);
+            this.BaudRateLabel.Name = "BaudRateLabel";
+            this.BaudRateLabel.Size = new System.Drawing.Size(71, 17);
+            this.BaudRateLabel.TabIndex = 4;
+            this.BaudRateLabel.Text = "BaudRate";
+            // 
+            // BaudRateComboBox
+            // 
+            this.BaudRateComboBox.FormattingEnabled = true;
+            this.BaudRateComboBox.Location = new System.Drawing.Point(365, 49);
+            this.BaudRateComboBox.Name = "BaudRateComboBox";
+            this.BaudRateComboBox.Size = new System.Drawing.Size(121, 24);
+            this.BaudRateComboBox.TabIndex = 5;
+            // 
+            // ParityLabel
+            // 
+            this.ParityLabel.AutoSize = true;
+            this.ParityLabel.Location = new System.Drawing.Point(538, 13);
+            this.ParityLabel.Name = "ParityLabel";
+            this.ParityLabel.Size = new System.Drawing.Size(44, 17);
+            this.ParityLabel.TabIndex = 6;
+            this.ParityLabel.Text = "Parity";
+            // 
+            // ParityComboBox
+            // 
+            this.ParityComboBox.FormattingEnabled = true;
+            this.ParityComboBox.Location = new System.Drawing.Point(541, 49);
+            this.ParityComboBox.Name = "ParityComboBox";
+            this.ParityComboBox.Size = new System.Drawing.Size(121, 24);
+            this.ParityComboBox.TabIndex = 7;
+            // 
+            // DataBitLabel
+            // 
+            this.DataBitLabel.AutoSize = true;
+            this.DataBitLabel.Location = new System.Drawing.Point(706, 12);
+            this.DataBitLabel.Name = "DataBitLabel";
+            this.DataBitLabel.Size = new System.Drawing.Size(65, 17);
+            this.DataBitLabel.TabIndex = 8;
+            this.DataBitLabel.Text = "Data Bits";
+            // 
+            // DataBitComboBox
+            // 
+            this.DataBitComboBox.FormattingEnabled = true;
+            this.DataBitComboBox.Location = new System.Drawing.Point(709, 47);
+            this.DataBitComboBox.Name = "DataBitComboBox";
+            this.DataBitComboBox.Size = new System.Drawing.Size(121, 24);
+            this.DataBitComboBox.TabIndex = 9;
+            // 
+            // DataTextBox
+            // 
+            this.DataTextBox.Location = new System.Drawing.Point(279, 153);
+            this.DataTextBox.Name = "DataTextBox";
+            this.DataTextBox.Size = new System.Drawing.Size(510, 22);
+            this.DataTextBox.TabIndex = 10;
+            // 
+            // ControlLabel
+            // 
+            this.ControlLabel.AutoSize = true;
+            this.ControlLabel.Location = new System.Drawing.Point(172, 241);
+            this.ControlLabel.Name = "ControlLabel";
+            this.ControlLabel.Size = new System.Drawing.Size(32, 17);
+            this.ControlLabel.TabIndex = 11;
+            this.ControlLabel.Text = "000";
+            // 
+            // DataLabel
+            // 
+            this.DataLabel.AutoSize = true;
+            this.DataLabel.Location = new System.Drawing.Point(328, 241);
+            this.DataLabel.Name = "DataLabel";
+            this.DataLabel.Size = new System.Drawing.Size(46, 17);
+            this.DataLabel.TabIndex = 12;
+            this.DataLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 577);
+            this.Controls.Add(this.DataLabel);
+            this.Controls.Add(this.ControlLabel);
+            this.Controls.Add(this.DataTextBox);
+            this.Controls.Add(this.DataBitComboBox);
+            this.Controls.Add(this.DataBitLabel);
+            this.Controls.Add(this.ParityComboBox);
+            this.Controls.Add(this.ParityLabel);
+            this.Controls.Add(this.BaudRateComboBox);
+            this.Controls.Add(this.BaudRateLabel);
+            this.Controls.Add(this.PortNameLabel);
+            this.Controls.Add(this.PortNameComboBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.ComboBox PortNameComboBox;
+        private System.Windows.Forms.Label PortNameLabel;
+        private System.Windows.Forms.Label BaudRateLabel;
+        private System.Windows.Forms.ComboBox BaudRateComboBox;
+        private System.Windows.Forms.Label ParityLabel;
+        private System.Windows.Forms.ComboBox ParityComboBox;
+        private System.Windows.Forms.Label DataBitLabel;
+        private System.Windows.Forms.ComboBox DataBitComboBox;
+        private System.Windows.Forms.TextBox DataTextBox;
+        private System.Windows.Forms.Label ControlLabel;
+        private System.Windows.Forms.Label DataLabel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static bool _continue;
+        static SerialPort _serialPort;
+        //static StringComparer _stringComparer;
+        //static Thread _readThread;
+        public Form1()
+        {
+            InitializeComponent();
+            _serialPort = new SerialPort();
+            //_stringComparer = StringComparer.OrdinalIgnoreCase;
+            //_readThread = new Thread(Read);
+        }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            
+            string[] baudRates = new string[] { "19200", "9600" };
+            BaudRateComboBox.Items.AddRange(baudRates);
+            BaudRateComboBox.SelectedIndex = 1;
+
+            string[] dataBits = new string[] { "5", "6","7","8"};
+            DataBitComboBox.Items.AddRange(dataBits);
+            DataBitComboBox.SelectedIndex = 3;
+
+     
+
+            //_serialPort = new SerialPort();
+
+            _serialPort.PortName = SetPortName(_serialPort.PortName, PortNameComboBox);
+            _serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate, BaudRateComboBox);
+            _serialPort.Parity = SetPortParity(_serialPort.Parity, ParityComboBox);
+            _serialPort.DataBits = SetPortDataBits(_serialPort.DataBits, DataBitComboBox);
+
+            //bunları eklemesek de olur
+            _serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
+            _serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
+
+            _serialPort.ReadTimeout = 500;
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            if (this.DataTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.DataTextBox.Text = text;
+            }
+        }
+
+        
+        public void Read(object sender, SerialDataReceivedEventArgs e)
+        {
+            string message = _serialPort.ReadExisting().ToString();
+            ShowRead(message);
+        }
+        
+        public void ShowRead(string data)
+        {
+            SetText(data);
+        }
+        public static string SetPortName(string defaultPortName,ComboBox comboBox)
+        {
+            string portName;
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox.Items.Add(s);
+            }
+            comboBox.SelectedIndex = 1;
+            portName = comboBox.SelectedIndex.ToString();
+
+            if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+            {
+                portName = defaultPortName;
+            }
+            return portName;
+        }
+
+        public static int SetPortBaudRate(int defaultPortBaudRate, ComboBox comboBox)
+        {
+            string baudRate;
+
+            baudRate = comboBox.SelectedItem.ToString();
+
+            if (baudRate == "")
+            {
+                baudRate = defaultPortBaudRate.ToString();
+            }
+
+            return int.Parse(baudRate);
+        }
+
+        public static Parity SetPortParity(Parity defaultPortParity, ComboBox comboBox)
+        {
+            string parity;
+
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                comboBox.Items.Add(s);
+            }
+            comboBox.SelectedIndex = 0; 
+
+            parity = comboBox.SelectedIndex.ToString();
+
+            if (parity == "")
+            {
+                parity = defaultPortParity.ToString();
+            }
+
+            return (Parity)Enum.Parse(typeof(Parity), parity, true);
+        }
+
+        public static int SetPortDataBits(int defaultPortDataBits, ComboBox comboBox)
+        {
+            string dataBits;
+
+            dataBits = comboBox.SelectedItem.ToString();
+
+            if (dataBits == "")
+            {
+                dataBits = defaultPortDataBits.ToString();
+            }
+
+            return int.Parse(dataBits.ToUpperInvariant());
+        }
+
+        public static StopBits SetPortStopBits(StopBits defaultPortStopBits)
+        {
+            string stopBits = "";
+
+            //bu kısım duruma göre ayarlanabilir !!!
+            /*
+            Console.WriteLine("Available StopBits options:");
+            foreach (string s in Enum.GetNames(typeof(StopBits)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+            
+            Console.Write("Enter StopBits value (None is not supported and \n" +
+             "raises an ArgumentOutOfRangeException. \n (Default: {0}):", defaultPortStopBits.ToString());
+            stopBits = Console.ReadLine();
+            */
+            if (stopBits == "")
+            {
+                stopBits = defaultPortStopBits.ToString();
+            }
+
+            return (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
+        }
+        public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
+        {
+            string handshake ="";
+            // burası duruma göre ayarlanabilir
+            /*
+            Console.WriteLine("Available Handshake options:");
+            foreach (string s in Enum.GetNames(typeof(Handshake)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Enter Handshake value (Default: {0}):", defaultPortHandshake.ToString());
+            handshake = Console.ReadLine();
+            */
+            if (handshake == "")
+            {
+                handshake = defaultPortHandshake.ToString();
+            }
+
+            return (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(Read);
+            _serialPort.Open();
+
+            _continue = true;
+            ControlLabel.Text = "Acik";
+            //_readThread.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            //_readThread.Join();
+            ControlLabel.Text = "Kapali";
+
+            _continue = false;
+            _serialPort.Close();
+        }
+    }
+}
